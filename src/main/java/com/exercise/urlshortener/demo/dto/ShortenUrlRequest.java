04026d434d2ad8330661d32de15ee10f683c1e24
dennis <dionisio.adan@googlemail.com>
2026-06-24 00:07:08 +0100

    refactor: use Lombok for ShortenUrlRequest

--- src/main/java/com/exercise/urlshortener/demo/dto/ShortenUrlRequest.java
+++ src/main/java/com/exercise/urlshortener/demo/dto/ShortenUrlRequest.java
@@ -1,29 +1,15 @@
 package com.exercise.urlshortener.demo.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class ShortenUrlRequest {
-
     // @NotBlank ensures the string is not null, not empty, and not just whitespace
     @NotBlank(message = "fullUrl is required")
     private String fullUrl;
 
     private String customAlias; // This is optional, according to the openapi.yaml
-
-    // Getters and Setters
-    public String getFullUrl() {
-        return fullUrl;
-    }
-
-    public void setFullUrl(String fullUrl) {
-        this.fullUrl = fullUrl;
-    }
-
-    public String getCustomAlias() {
-        return customAlias;
-    }
-
-    public void setCustomAlias(String customAlias) {
-        this.customAlias = customAlias;
-    }
 }
