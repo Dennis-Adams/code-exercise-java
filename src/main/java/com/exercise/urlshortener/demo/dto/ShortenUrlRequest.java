5c67135f25924bc1ffc6e6a0e9bdfcd36a0425fe
dennis <dionisio.adan@googlemail.com>
2026-06-25 12:23:22 +0100

    fix: implement strict Java Bean validation for URL and custom alias, returning 400 Bad Request

--- src/main/java/com/exercise/urlshortener/demo/dto/ShortenUrlRequest.java
+++ src/main/java/com/exercise/urlshortener/demo/dto/ShortenUrlRequest.java
@@ -1,6 +1,7 @@
 package com.exercise.urlshortener.demo.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,7 +10,17 @@ import lombok.Setter;
 public class ShortenUrlRequest {
     // @NotBlank ensures the string is not null, not empty, and not just whitespace
     @NotBlank(message = "fullUrl is required")
+
+    // check URL format
+    @Pattern(
+            regexp = "^https?://(localhost|(www\\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6})\\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)$",
+            message = "Invalid URL format")
+
     private String fullUrl;
 
+    @Pattern(
+            regexp = "^[a-zA-Z0-9-_]+$",
+            message = "Invalid input: Custom alias must contain only letters, numbers, hyphens, or underscores"
+    )
     private String customAlias; // This is optional, according to the openapi.yaml
 }
